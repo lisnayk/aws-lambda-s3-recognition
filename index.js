3ff252a37717ad3cf61a3b5268c5a8aaa88ca0b0
Andrii Lisnyak <andrey.lisnyak@gmail.com>
2023-04-05 20:56:24 +0300

    Avoid empty vehicle information

--- index.js
+++ index.js
@@ -23,13 +23,15 @@ module.exports.handler = async (event) => {
         let plate = await plateService.getPlateInfo(Buffer.from(originImage.Body).toString('base64'));
         if (plate.results.length > 0) {
             vehicle = await getVehicleInfo(plate.results[0].plate);
-            await store({
-                device_id: srcKey,
-                time: new Date().toISOString(),
-                picture: s3Thumbnail,
-                plate: JSON.stringify(plate),
-                vehicle: JSON.stringify(vehicle)
-            });
+            if (Object.keys(vehicle).length > 0) {
+                await store({
+                    device_id: srcKey,
+                    time: new Date().toISOString(),
+                    picture: s3Thumbnail,
+                    plate: JSON.stringify(plate),
+                    vehicle: JSON.stringify(vehicle)
+                });
+            }
         }
         await notifyService.notify({...event.Records[0].s3, plate, vehicle});
     } catch (error) {
@@ -52,7 +54,7 @@ function validateImage(srcKey) {
     }
     // Check that the image type is supported
     const imageType = typeMatch[1].toLowerCase();
-    if (imageType !=="jpg" && imageType !== "png") {
+    if (imageType !== "jpg" && imageType !== "png") {
         throw new Error(`Unsupported image type: ${imageType}`);
     }
 }
